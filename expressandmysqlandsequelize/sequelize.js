d2c43e5e1ddfe8ed3125d7d3490317b633630a68
Ryan Gabryel <85703424+rlimazzz@users.noreply.github.com>
2024-02-22 13:59:13 -0300

    Create sequelize.js

---
+++ expressandmysqlandsequelize/sequelize.js
@@ -0,0 +1,39 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('pessoas', 'root', '12345', {
+    host: "localhost",
+    dialect: "mysql"
+});
+
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING,
+    },
+    conteudo: {
+        type: Sequelize.TEXT,
+    }
+});
+
+//Postagem.sync({force : true});
+//comando que faz eu criar uma tabela no meu banco de dados mysql
+
+const Usuario = sequelize.define('usuarios', {
+    nickname: {
+        type: Sequelize.STRING,
+    },
+    email: {
+        type: Sequelize.TEXT,
+    },
+    idade: {
+        type: Sequelize.TINYINT,
+    }
+});
+
+//Usuario.sync({force : true});
+
+Usuario.create({
+    nickname: "xdryanxd",
+    email: "blablabla",
+    idade: 22
+});
+
+//criando um usuario na table, com o comando create, muito fácil
